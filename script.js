15f5e05f0756a37a7cb7b096de9efaa468283174
Atuu <adrianudedi@gmail.com>
2026-02-28 13:31:25 +0200

    Improve password input handling in checkPassword function
    Enhance password check function to normalize input by removing extra spaces and ignoring case.

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 // Password check for page 1
 function checkPassword() {
     const input = document.getElementById('passwordInput').value;
+      .getElementById("passwordInput")
+  .value
+  .replace(/\s+/g, " ")   // remove extra spaces inside
+  .trim()                 // remove spaces at start & end
+  .toLowerCase();         // ignore capital letters
     const correctPassword = "6 August";
-
-    if (input === correctPassword) {
+     if (input === correctPassword) {
         window.location.href = "songs.html";
     } else {
         alert("Incorrect password");
     }
 }
+
 
